3c07c53ec6c5e5a13ff59b5acf04cdd4848943cc
JinHyeon <tiemo0708@naver.com>
2025-10-27 19:11:17 +0900

    feat: 예외처리 에러메시지 상수 클래스 구현

---
+++ src/main/java/racingcar/error/ErrorMessage.java
@@ -0,0 +1,15 @@
+package racingcar.error;
+
+public class ErrorMessage {
+    private static final String ERROR_PREFIX = "[ERROR] ";
+
+    public static final String CAR_NAME_NULL_OR_EMPTY = ERROR_PREFIX + "자동차 이름은 비어 있을 수 없습니다.";
+    public static final String CAR_NAME_TOO_LONG = ERROR_PREFIX + "자동차 이름은 5자를 넘을 수 없습니다: ";
+    public static final String INVALID_TRIALS_NUMBER = ERROR_PREFIX + "시도 횟수는 0보다 커야 합니다.";
+    public static final String TRIALS_NOT_A_NUMBER = ERROR_PREFIX + "시도 횟수는 유효한 정수여야 합니다.";
+    public static final String CAR_NAME_DUPLICATE = ERROR_PREFIX + "자동차 이름은 중복 될 수 없습니다";
+
+    private void ErrorMessages() {
+
+    }
+}
