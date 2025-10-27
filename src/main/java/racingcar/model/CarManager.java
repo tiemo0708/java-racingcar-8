5510590de08c2a2d73778ba23170243719673cec
JinHyeon <tiemo0708@naver.com>
2025-10-27 19:44:26 +0900

    feat: 자동차 관리 CarManager 추가 (자동차 초기화 및 이동 처리)

---
+++ src/main/java/racingcar/model/CarManager.java
@@ -0,0 +1,30 @@
+package racingcar.model;
+
+import racingcar.generator.RandomNumberGenerator;
+
+import java.util.List;
+
+public class CarManager {
+    private final RandomNumberGenerator randomNumberGenerator;
+    private Cars cars;
+
+    public CarManager(RandomNumberGenerator randomNumberGenerator) {
+        this.randomNumberGenerator = randomNumberGenerator;
+    }
+
+    public void initializeCars(String carNames) {
+        this.cars = Cars.fromNames(carNames);
+    }
+
+    public void playRound() {
+        cars.playRound(randomNumberGenerator);
+    }
+
+    public List<String> getCurrentRoundResults() {
+        return cars.getCurrentRoundResults();
+    }
+
+    public List<String> getWinners() {
+        return cars.getWinners();
+    }
+}
